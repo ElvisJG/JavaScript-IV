de8d15eb6f7473a18ac7bd0e5979e36faefcebaf
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-04-23 20:50:00 -0400

    work in progress

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,35 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(personAttributes) {
+    this.name = personAttributes.name;
+    this.age = personAttributes.age;
+    this.location = personAttributes.location;
+    this.gender = personAttributes.gender;
+  }
+  speak() {
+    return `My name is ${this.name} and I love Lambda!`;
+  }
+}
+
+class Instructor extends Person {
+  constructor(instructorAttributes) {
+    super(instructorAttributes);
+    this.specialty = instructorAttributes.specialty;
+    this.favLanguage = instructorAttributes.favLanguage;
+    this.catchPhrase = instructorAttributes.catchPhrase;
+  }
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+  grade(student, subject) {
+    console.log(`${student.name} recieves a perfect score on ${subject}`);
+  }
+}
+
+class Student extends Person {
+  constructor(studentAttributes) {
+    super(studentAttributes);
+    this.previousBackground = studentAttributes.previousBackground;
+  }
+}
